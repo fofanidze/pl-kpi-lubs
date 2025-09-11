a8da998c0f629dbb24220ef036638a8ec5583ee1
romanchuk <platontop54@gmail.com>
2025-09-11 13:47:20 +0300

    my second commit

--- codes 2/3.py
+++ codes 2/3.py
@@ -1,2 +1,4 @@
-for i in range(1,300):
-    print(i)
+for i in range(1,301):
+    if i % 5 != 0:
+        print(i)
+    
---
+++ codes 2/4.py
@@ -0,0 +1,6 @@
+n = 5 # int(input("n = "))
+for i in range(n):
+    if i % 7 == 0:
+        elif:
+            
+        print(i)
